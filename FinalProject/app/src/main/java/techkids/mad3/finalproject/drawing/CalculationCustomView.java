45c44fa1af8f5fd702e6685b2c23c1ef711bff30
hoangduong97 <hoang_duong_1997@yahoo.com>
2016-05-14 19:13:04 +0700

    no message

--- FinalProject/app/src/main/java/techkids/mad3/finalproject/drawing/CalculationCustomView.java
+++ FinalProject/app/src/main/java/techkids/mad3/finalproject/drawing/CalculationCustomView.java
@@ -11,17 +11,18 @@ import android.view.View;
  * Created by HoangDuong1607 on 5/14/2016.
  */
 public class CalculationCustomView extends View {
-    private final float TEXT_SIZE = 100;
+    private final float TEXT_SIZE_DP = 48;
     private final float STROKE_WIDTH = 5;
 
+    private float TEXT_SIZE_PX;
     private float VIEW_WIDTH;
     private float VIEW_HEIGHT;
 
     private Paint paint = new Paint();
 
-    private int firstNumber;
-    private int secondNumber;
-    private String operator;
+    private int firstNumber = 0;
+    private int secondNumber = 0;
+    private String operator = "+";
 
     public CalculationCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -30,25 +31,27 @@ public class CalculationCustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float factor = getResources().getDisplayMetrics().density;
+        TEXT_SIZE_PX = TEXT_SIZE_DP * factor;
         VIEW_WIDTH = getWidth();
         VIEW_HEIGHT = getHeight();
         paint.setColor(Color.WHITE);
         paint.setStrokeWidth(STROKE_WIDTH);
-        paint.setTextSize(TEXT_SIZE);
+        paint.setTextSize(TEXT_SIZE_PX);
         paint.setTextAlign(Paint.Align.LEFT);
 //        Draw operator
-        canvas.drawText("+", 0, 3 * TEXT_SIZE / 2, paint);
+        canvas.drawText(this.operator, 0, 3 * TEXT_SIZE_PX / 2, paint);
 
         paint.setTextAlign(Paint.Align.RIGHT);
 
 //        Draw first number
-        canvas.drawText("162", VIEW_WIDTH, TEXT_SIZE, paint);
+        canvas.drawText("" + this.firstNumber, VIEW_WIDTH, TEXT_SIZE_PX, paint);
 
 //        Draw second number
-        canvas.drawText("21", VIEW_WIDTH, 2 * TEXT_SIZE, paint);
+        canvas.drawText("" + this.secondNumber, VIEW_WIDTH, 2 * TEXT_SIZE_PX, paint);
 
 //        Draw line
-        canvas.drawLine(0, 5 * TEXT_SIZE / 2, VIEW_WIDTH, 5 * TEXT_SIZE / 2, paint);
+        canvas.drawLine(0, 5 * TEXT_SIZE_PX / 2, VIEW_WIDTH, 5 * TEXT_SIZE_PX / 2, paint);
     }
 
     public int getFirstNumber() {
