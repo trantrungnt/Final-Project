5b494c1961dc3a4a6206c02d93f9aabbd131bf43
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-14 12:10:04 +0700

    Final Project

---
+++ FinalProject/app/src/androidTest/java/techkids/mad3/finalproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package techkids.mad3.finalproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ FinalProject/app/src/main/java/techkids/mad3/finalproject/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package techkids.mad3.finalproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import techkids.mad3.finalproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
